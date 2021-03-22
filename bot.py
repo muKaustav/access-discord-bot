53d59cd56e677578e568de5f3109dbc8fcfded93
kaussy6902 <mu.kaustav@gmail.com>
2021-03-22 17:06:43 +0530

    upd2

--- bot.py
+++ bot.py
@@ -149,10 +149,10 @@ async def get_data(ctx):
 @client.command(name='project')
 async def project(ctx):
     global takenGuild
-    takenGuild = client.get_guild(711418582302720084)
+    takenGuild = client.get_guild(809275451322269728)
 
     global category
-    category = client.get_channel(822733836122062849)
+    category = client.get_channel(809286493272801290)
 
     myEmbed = discord.Embed(
         title="CREATE A NEW PROJECT", color=0xff3838)
