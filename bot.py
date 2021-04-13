1dc74dea883108f2c1d6aa9da1cdab179a844654
kaussy6902 <mu.kaustav@gmail.com>
2021-04-13 13:50:03 +0530

    gif update

--- bot.py
+++ bot.py
@@ -1,12 +1,26 @@
 import discord
 from discord.ext import commands, tasks
 from discord.utils import get
+import pandas as pd
 import os
 from bs4 import BeautifulSoup
 import requests
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix="--", intents=intents)
+
+
+@client.command(name='e')
+async def pepe(ctx, arg1):
+
+    url = "https://raw.githubusercontent.com/muKaustav/access-discord-bot/main/emoji.csv"
+    df = pd.read_csv(url, sep=",")
+
+    for line in df['SRC']:
+        if str(arg1).lower() in line.lower():
+            x = line.split(",")
+            await ctx.send(x[0])
+            break
 
 
 @client.command(name='count')
